cb33c2b02e3596802fe05cd1f7b40eba50f90c27
jangbokyung <bokyungj911@gmail.com>
2024-11-14 15:15:50 +0900

    [chap01] 첫 번째 코딩 테스트

---
+++ chap01_입출력과 사칙연산/chap01.py
@@ -0,0 +1,67 @@
+#01) 2557. 	Hello World 
+print('Hello World!')
+
+#02) 1000. 	A+B
+a, b = map(int, input().split())
+print(a+b)
+
+#03) 1001. 	A+B
+a, b = map(int, input().split())
+print(a+b)
+
+#04) 10998. A×B
+a, b = map(int, input().split())
+print(a*b)
+
+#05) 1008. A/B
+a, b = map(int, input().split())
+print(a/b)
+
+#06) 10869. 사칙연산
+a, b = map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+#07) 10926. ??!
+id = input()
+print (id + "??!")
+
+#08) 18108.  1998년생인 내가 태국에서는 2541년생?!
+y = int(input())
+print (y - 543)
+
+#09) 10430. 나머지
+A,B,C = map(int,input().split())
+
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print( ((A%C) * (B%C))%C)
+       
+#10) 2588. 	곱셈
+A = int(input())
+B = int(input())
+print(A * (B % 10))
+print(A * ((B % 100) // 10))
+print(A * (B // 100))
+print(A*B)
+
+#11) 11382. 꼬마 정민
+A,B,C = map(int,input().split())
+print(A + B + C)
+
+#12) 10171. 고양이
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
+
+#13) 10172. 개
+print("|\\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
